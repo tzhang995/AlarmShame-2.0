d9ce75706b14fa4dea212901c12394acf22c5eb7
Tony <tzhang995@gmail.com>
2015-12-27 17:32:26 -0500

    Ability to choose when you want the alarm

--- app/src/main/java/com/example/tony/alarmshame/MainActivity.java
+++ app/src/main/java/com/example/tony/alarmshame/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.tony.alarmshame;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +12,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int dialog_id = 0;
+    int hour,minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+
     }
 
     @Override
@@ -42,4 +56,25 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //create a time picker
+    public void addTime(View view){
+        showDialog(dialog_id);
+    }
+    protected Dialog onCreateDialog(int id){
+        switch(id) {
+            case dialog_id:
+                return new TimePickerDialog(this, mTimeSetListener, hour, minute, false);
+        }
+        return null;
+    }
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int hour_minute) {
+            hour = hourOfDay;
+            minute = hour_minute;
+            Toast.makeText(getBaseContext(),"Alarm is set to " + hour+ ":" +minute, Toast.LENGTH_LONG).show();
+        }
+    };
 }
+
